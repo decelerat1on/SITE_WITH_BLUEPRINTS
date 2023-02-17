868d56def0427d979f64e283aa2771f2621f4fc7
arabr <ohio.ivan@yandex.ru>
2023-02-17 20:00:34 +0300

    Error 404 mainpage.py

--- mainpage/mainpage.py
+++ mainpage/mainpage.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template
 
-mainpage_blueprint = Blueprint('mainpage_blueprint', __name__, template_folder='templates')
+mainpage_blueprint = Blueprint('mainpage_blueprint', __name__, template_folder='templates', static_folder='static')
 
 @mainpage_blueprint.route('/')
 def mainpage_page():
-    return render_template('input.html')
+    return render_template('mainpage.html')
